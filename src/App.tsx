9fbd3dc814dcad0642dff3185e7e72d1c2fd2017
Yanis REMILA <139234182+yanis-remila@users.noreply.github.com>
2024-11-21 15:03:31 +0100

    Version 2 OK.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import EmployeeDashboard from "./views/EmployeeDashboard";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import FreelanceDashboard from "./views/FreelanceDashboard";
